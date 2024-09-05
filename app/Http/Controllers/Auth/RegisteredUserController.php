3ca342176779eecfdbefae4eff60bfe56fbd3284
darren <darren.kooyz@gmail.com>
2024-09-05 17:20:41 +0800

    feat: otp

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -151,8 +151,8 @@ class RegisteredUserController extends Controller
 
         // Auth::login($user);
 
-        // $smsService = new Sms123Service();
-        // $smsService->sendOtp($formattedPhone, $otp);
+        $smsService = new Sms123Service();
+        $smsService->sendOtp($formattedPhone, $otp);
 
         // return redirect(route('dashboard', absolute: false));
         return redirect(route('otp'));
